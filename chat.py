156866d2b5995c72a90a7662f1038fa7dcdca492
Lucas <121887128+lucas-gab46@users.noreply.github.com>
2025-03-06 15:19:58 -0500

    chat.py

--- chat.py
+++ chat.py
@@ -1,32 +1,30 @@
 import os
 
 mensagens = []
+import os
+
+mensagens = []
 
 nome = input("Nome: ")
 
 while True:
-  
- os.system('cls')
-
-if len(mensagens) > 0:
-    for m in mensagens:
+    os.system('cls')  
     
-      print(m['nome'], "-", m ['texto'])
-print("_____")
-
-texto = input("Mensagem : ")
-
-if texto == "fim":
-     break
-
-
-mensagens.apppend({
-  
-"nome":  nome,
-"texto":  texto,
-
-})
-  
+    if len(mensagens) > 0:
+        for m in mensagens:
+            print(m['nome'], "-", m['texto'])
+    
+    print("_____")
+    
+    texto = input("Mensagem : ")
+    
+    if texto == "fim":
+        break
+    
+    mensagens.append({
+        "nome": nome,
+        "texto": texto,
+    })
 
 
 
